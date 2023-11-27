84e6850729d6c01f49ed7f416e773eaa09e42f11
Bernard Towindo <bernardtowindo38@gmail.com>
2023-11-27 19:32:55 +0200

    Homepage

--- app/index.js
+++ app/index.js
@@ -1,11 +1,46 @@
 import { View, Text} from 'react-native';
+import { View , Scrollview, SafeAreaView } from 'react-native';
+import { Stack useRouter } from 'expo-router';
+import { COLORS, icons,images,SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn,Welcome} from '../components';
+import React from 'react';
 
 const Home = () =>{
 
+    const router = useRouter();
+
     return(
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView  style{{flex: 1, background: COLORS.lightWhite}} >
+           <Stack.Screen
+           options ={{
+
+            headerStyle: { backgroundColor: COLORS.lightWhite},
+            headerShadowVisible: false,
+            headerLeft: () =>(
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension ="60%"/>
+            ),
+
+            headerRightt: () =>(
+                <ScreenHeaderBtn iconUrl={icons.profile} dimension ="100%"/>
+            ),
+            headerTitle: ""
+           }}
+           />
+
+           <Scrollview  showsVerticalScrollIndcator ={false}>
+
+            <View 
+             style={{
+                flex: 1,
+                padding: SIZES.medium
+             }}>
+
+                <Welcome/>
+             </View>
+
+           </Scrollview>
+
+        </SafeAreaView>
     )
 }
 
